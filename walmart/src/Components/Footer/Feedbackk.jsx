9058dc5fc8e9236e862fa12516154eadee97d6b5
payal bhattamisra <payalbhattamisra@gmail.com>
2024-08-04 18:22:22 +0530

    push

---
+++ walmart/src/Components/Footer/Feedbackk.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Feedback.css"
+const Feedbackk=()=>{
+return (
+    <>
+    <div className="feedback">
+        <p>We’d love to hear what you think!</p>
+        <div className="feed"> 
+        <button>Give Feedback</button>
+        </div>
+    </div>
+    </>
+)
+}
+export default Feedbackk
